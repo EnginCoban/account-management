b06102bf8fcb113444ca852a815ab591b5a34254
EnginCoban <141469133+EnginCoban@users.noreply.github.com>
2025-01-25 22:28:09 +0100

    inline styles entfernt

--- registerMe.php
+++ registerMe.php
@@ -11,28 +11,7 @@ include 'connectionDB.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <style>
-        * {
-            padding: 0px;
-            margin: 0px;
-        }
-
-        form {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            flex-direction: column;
-            margin-top: 50px;
-            gap: 20px;
-            background-color: black;
-            color: white;
-            width: 30%;
-            margin: auto;
-            margin-top: 30px;
-            padding: 30px;
-            border-radius: 20px
-        }
-    </style>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>

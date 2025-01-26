5974e70637a0a8418ebc7f275bfe3cf046438a8d
EnginCoban <141469133+EnginCoban@users.noreply.github.com>
2025-01-26 19:28:18 +0100

    stylesheet und bootstrap hinzugefügt

--- change-data.php
+++ change-data.php
@@ -13,6 +13,8 @@ include 'connection-db.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
@@ -33,18 +35,17 @@ include 'connection-db.php';
                         <label for="exampleInputPassword1" class="form-label">Code</label>
                         <input type="text" class="form-control" id="exampleInputPassword1" name="code" value=<?= $_SESSION['randomCode']; ?>>
                     </div>
-                    <div style="visibility: hidden;">
-                        <label for="exampleInputPassword1" class="form-label">Email</label>
-                        <input type="email" class="form-control" id="exampleInputPassword1" name="email" value=<?= $_SESSION['email']; ?>>
-                    </div>
+                    
 
-                    <button type="submit" class="btn btn-primary" name="change">change Data</button>
+                    <button type="submit" class="btn btn-primary" name="change">Daten ändern</button>
+                    <?php include 'back-button.php'; ?>
 
                 </form>
             </div>
         </div>
 
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 </body>
 

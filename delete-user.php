76b447103e469390dcb065a57b1d95d59626f8a3
EnginCoban <141469133+EnginCoban@users.noreply.github.com>
2025-01-26 20:23:27 +0100

    stylesheet hinzugefügt, back-button hinzugefügt

--- delete-user.php
+++ delete-user.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include 'connection-db.php';
 
 
@@ -8,32 +8,36 @@ include 'connection-db.php';
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-<div class="container">
+    <div class="container">
         <div class="row">
             <div class="col">
                 <form action="database.php" method="POST">
                     <div>
                         <label for="exampleInputEmail1" class="form-label">Email-Adresse</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" >
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email">
                     </div>
-                   
+
 
                     <button type="submit" class="btn btn-primary" name="delete">Account löschen</button>
-                   
+                    <?php include 'back-button.php'; ?>
+
                 </form>
             </div>
         </div>
 
     </div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 </body>
+
 </html>

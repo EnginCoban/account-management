80a90dfcc6655e643b9d6b70e68b5b7687de0058
EnginCoban <141469133+EnginCoban@users.noreply.github.com>
2025-01-26 20:24:54 +0100

    back-button, stylesheet hinzugefügt

--- forgot-password.php
+++ forgot-password.php
@@ -10,7 +10,7 @@ include 'connection-db.php';
     <meta name="viewport" content="width=#, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
@@ -26,7 +26,7 @@ include 'connection-db.php';
 
 
                     <button type="submit" class="btn btn-primary" name="forgot">Code anfordern</button>
-
+                    <?php include 'back-button.php'; ?>
 
                 </form>
             </div>
